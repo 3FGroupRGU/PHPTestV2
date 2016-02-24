95b731d5b0e3fc8e2ebd1b507fc4a451772f2425
1300427 <1300427@rgu.ac.uk>
2016-02-24 16:08:13 +0000

    additional change 11

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
         <p>
             <?php
                 echo "Hello World";
+            ini_set('display_errors', 1);
+            ini_set('display_startup_errors', 1);
+            error_reporting(E_ALL);
             ?>
             <br>
             <?php
