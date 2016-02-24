acb38d7991ad2591f1cdb464fc269a520dcfd319
1300427 <1300427@rgu.ac.uk>
2016-02-24 16:26:43 +0000

    additional change 15

--- index.php
+++ index.php
@@ -99,7 +99,7 @@
             ?>
         <br>
             <?php
-                $myArray = array ("do", "re", "mi"); //declares the array
+                $myArray = array("do", "re", "mi"); //declares the array
                 echo $myArray[0]; //outputs "do"
                 $myArray[1] = "la"; // modifies position 1 (re)
                 echo $myArray[1]; // outpute "la"
