99890fa770d0711d4dd5469efcf9b070da338909
1300427 <1300427@rgu.ac.uk>
2016-03-02 15:15:30 +0000

    additional change 21

---
+++ week5/connect.php
@@ -0,0 +1,14 @@
+<?php
+define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
+define('DB_USERNAME', 'b679b44245bd');
+define('DB_PASSWORD', 'c0bf782c');
+define('DB_DATABASE', 'CMM503_1300427');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
+
+/**
+ * Created by PhpStorm.
+ * User: 1300427
+ * Date: 02/03/2016
+ * Time: 14:53
+ */
